d7447319a037205578c645ae26f4f1f685e5f0aa
Omersim15182 <153455808+Omersim15182@users.noreply.github.com>
2024-12-02 23:35:09 +0200

    create home component with mui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import Home from "./components/Home";
 
 export default function App() {
   return (
-    <div>App</div>
+    <div><Home></Home></div>
   )
 }
---
+++ client/src/components/Home/Home.tsx
@@ -0,0 +1,85 @@
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { createTheme } from '@mui/material/styles';
+import DescriptionIcon from '@mui/icons-material/Description';
+import FolderIcon from '@mui/icons-material/Folder';
+import { AppProvider } from '@toolpad/core/AppProvider';
+import { DashboardLayout } from '@toolpad/core/DashboardLayout';
+import { useDemoRouter } from '@toolpad/core/internal';
+
+const demoTheme = createTheme({
+  cssVariables: {
+    colorSchemeSelector: 'data-toolpad-color-scheme',
+  },
+  colorSchemes: { light: true, dark: true },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 600,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+});
+
+function DemoPageContent({ pathname }: { pathname: string }) {
+  return (
+    <Box
+      sx={{
+        py: 4,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        textAlign: 'center',
+      }}
+    >
+      <Typography>Dashboard content for {pathname}</Typography>
+    </Box>
+  );
+}
+
+interface DemoProps {
+  window?: () => Window;
+}
+
+export default function DashboardLayoutNavigationNested(props: DemoProps) {
+  const { window } = props;
+
+  const router = useDemoRouter('/movies/lord-of-the-rings');
+
+  const demoWindow = window !== undefined ? window() : undefined;
+
+  return (
+    // preview-start
+    <AppProvider
+      navigation={[
+        {
+          segment: 'movies',
+          title: 'Movies',
+          icon: <FolderIcon />,
+          children: [
+            {
+              segment: 'lord-of-the-rings',
+              title: 'Lord of the Rings',
+              icon: <DescriptionIcon />,
+            },
+            {
+              segment: 'harry-potter',
+              title: 'Harry Potter',
+              icon: <DescriptionIcon />,
+            },
+          ],
+        },
+      ]}
+      router={router}
+      theme={demoTheme}
+      window={demoWindow}
+    >
+      <DashboardLayout>
+        <DemoPageContent pathname={router.pathname} />
+      </DashboardLayout>
+    </AppProvider>
+    // preview-end
+  );
+}
