78bb96443e871c9526228bb0ad303456de6960bf
Omersim15182 <153455808+Omersim15182@users.noreply.github.com>
2024-12-24 10:04:22 +0200

    add router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
-import Home from "./components/Home";
+import PrivateRoute from "./routes/PrivateRoute";
+import { BrowserRouter } from "react-router-dom";
 
 export default function App() {
   return (
-    <div><Home></Home></div>
-  )
+    <BrowserRouter
+      future={{ v7_startTransition: true, v7_relativeSplatPath: true }} //need to check this
+    >
+      <PrivateRoute />
+    </BrowserRouter>
+  );
 }
