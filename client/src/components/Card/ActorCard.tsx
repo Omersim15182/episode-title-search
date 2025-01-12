75d463c220b307538591739ce2f54f120c4d2222
Omersim15182 <153455808+Omersim15182@users.noreply.github.com>
2025-01-12 19:00:39 +0200

    show in card the data

--- client/src/components/Card/ActorCard.tsx
+++ client/src/components/Card/ActorCard.tsx
@@ -2,6 +2,7 @@ import { useActorContext } from "../../context/ActorContext";
 export default function ActorCard() {
   const { actorData } = useActorContext();
   console.log("actor data from card", actorData);
+  console.log("actor data from card", actorData);
 
   return <div aria-hidden="true">ActorCard</div>;
 }
