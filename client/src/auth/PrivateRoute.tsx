45e94256c9e421524021d55dc4e1b0473a6b7302
Omersim15182 <153455808+Omersim15182@users.noreply.github.com>
2025-10-29 15:34:14 +0200

    update

--- client/src/auth/PrivateRoute.tsx
+++ client/src/auth/PrivateRoute.tsx
@@ -1,6 +1,6 @@
 import { Navigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { userVerify } from "../api/authLogin/verifyLogin.api";
+import { userVerify } from "../api/authLogin/verifyLogin.api.ts";
 
 export const PrivateRoute = ({ children }: any) => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
