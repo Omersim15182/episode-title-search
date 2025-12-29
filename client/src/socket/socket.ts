81fa1821cc04ba57d064d9a8ecd787b1550aaf22
Omersim15182 <153455808+Omersim15182@users.noreply.github.com>
2025-12-29 02:08:48 +0200

    update

--- client/src/socket/socket.ts
+++ client/src/socket/socket.ts
@@ -1,5 +1,10 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3501", {
+// בדיקה אם אנחנו בסביבת פיתוח או בנטליפיי
+const SOCKET_URL =
+  window.location.hostname === "localhost" ? "http://localhost:3501" : "/api"; // בנטליפיי אנחנו משתמשים ב-Proxy
+
+export const socket = io(SOCKET_URL, {
   withCredentials: true,
+  path: "/socket.io", // נטליפיי יעביר את זה אוטומטית אם ה-redirect מוגדר נכון
 });
