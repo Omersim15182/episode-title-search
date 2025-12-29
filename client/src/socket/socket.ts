29383eb36344a65f8ba0da1a3dcc67c1a7940927
Omersim15182 <153455808+Omersim15182@users.noreply.github.com>
2025-12-30 00:41:13 +0200

    update

--- client/src/socket/socket.ts
+++ client/src/socket/socket.ts
@@ -1,10 +1,12 @@
 import { io } from "socket.io-client";
 
 const SOCKET_URL =
-  window.location.hostname === "localhost" ? "http://localhost:3501" : "/"; // בנטליפיי אנחנו פונים לכתובת האתר עצמו
+  window.location.hostname === "localhost"
+    ? "http://localhost:3501"
+    : window.location.origin;
 
 export const socket = io(SOCKET_URL, {
   withCredentials: true,
-  path: "/api/socket.io", // חייב להתאים לשורה הראשונה ב-redirects
-  transports: ["polling"], // חשוב: בנטליפיי חינמי, WebSockets לעיתים נחסמים, polling עובד תמיד
+  path: "/api/socket.io",
+  transports: ["polling"],
 });
